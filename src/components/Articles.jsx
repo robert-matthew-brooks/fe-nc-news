7a81c0e40d1317c2f2de4ed5a589ddfaf1af9088
bob <robert.matthew.brooks@gmail.com>
2023-07-21 12:06:39 +0100

    trying to stop merge errors in vscode

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -51,7 +51,7 @@ export default function Articles() {
 
             try {
                 const { articles, total_count } = await fetchArticles(queries);
->>>>>>>>> Temporary merge branch 2
+
                 setArticles(articles);
                 setTotalArticles(total_count);
             }
